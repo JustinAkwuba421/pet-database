232cc602aab7b896a415ddf34670c40a99353f52
JustinAkwuba421 <jchukwuemeka@icloud.com>
2026-01-19 23:02:08 -0600

    Update PetDatabase.java

--- PetDatabase.java
+++ PetDatabase.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class PetDatabase {
-    private final ArrayList<Pet> pets = new ArrayList<>();
+
+    private ArrayList<Pet> pets = new ArrayList<>();
 
     public int size() {
         return pets.size();
@@ -12,70 +13,59 @@ public class PetDatabase {
     }
 
     public void showAllPets() {
-        printTableHeader();
+        printHeader();
         for (int i = 0; i < pets.size(); i++) {
             printRow(i, pets.get(i));
         }
-        printTableFooter(pets.size());
+        printFooter(pets.size());
     }
 
-    public void searchByName(String nameQuery) {
-        printTableHeader();
+
+
+    public void searchByName(String name) {
+        printHeader();
         int count = 0;
+
         for (int i = 0; i < pets.size(); i++) {
-            if (pets.get(i).getName().equalsIgnoreCase(nameQuery)) {
+            if (pets.get(i).getName().equalsIgnoreCase(name)) {
                 printRow(i, pets.get(i));
                 count++;
             }
         }
-        printTableFooter(count);
+
+        printFooter(count);
     }
 
-    public void searchByAge(int ageQuery) {
-        printTableHeader();
+    public void searchByAge(int age) {
+        printHeader();
         int count = 0;
+
         for (int i = 0; i < pets.size(); i++) {
-            if (pets.get(i).getAge() == ageQuery) {
+            if (pets.get(i).getAge() == age) {
                 printRow(i, pets.get(i));
                 count++;
             }
         }
-        printTableFooter(count);
+
+        printFooter(count);
     }
 
-    public boolean updatePet(int id, String newName, int newAge) {
-        if (id < 0 || id >= pets.size()) return false;
-        Pet p = pets.get(id);
-        String oldName = p.getName();
-        int oldAge = p.getAge();
 
-        p.setName(newName);
-        p.setAge(newAge);
-
-        System.out.println(oldName + " " + oldAge + " changed to " + newName + " " + newAge + ".");
-        return true;
-    }
-
-    public boolean removePet(int id) {
-        if (id < 0 || id >= pets.size()) return false;
-        Pet removed = pets.remove(id);
-        System.out.println(removed.getName() + " " + removed.getAge() + " is removed.");
-        return true;
-    }
-
-    private void printTableHeader() {
+    private void printHeader() {
         System.out.println("+----------------------+");
         System.out.printf("| %-3s| %-10s| %-4s|%n", "ID", "NAME", "AGE");
         System.out.println("+----------------------+");
     }
 
     private void printRow(int id, Pet pet) {
-        System.out.printf("| %-3d| %-10s| %-4d|%n", id, pet.getName(), pet.getAge());
+        System.out.printf("| %-3d| %-10s| %-4d|%n",
+                id, pet.getName(), pet.getAge());
     }
 
-    private void printTableFooter(int rows) {
+    private void printFooter(int rows) {
         System.out.println("+----------------------+");
         System.out.println(rows + " rows in set.");
     }
 }
+
 
