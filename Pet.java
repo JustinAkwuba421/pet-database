65842a8bd6095e3621df8cc62c846562373a8775
JustinAkwuba421 <jchukwuemeka@icloud.com>
2026-01-19 23:01:37 -0600

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -7,14 +7,19 @@ public class Pet {
         this.age = age;
     }
 
-    public String getName() { return name; }
-    public int getAge() { return age; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setAge(int age) { this.age = age; }
+    public int getAge() {
+        return age;
+    }
 
-    @Override
-    public String toString() {
-        return name + " " + age;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
